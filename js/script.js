a8ccfeae1756a769f7f29f8fcdcbd5fc5e92abba
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-06 13:34:42 +0200

    intestazione

---
+++ js/script.js
@@ -0,0 +1,4 @@
+/*Visualizzare in pagina 5 numeri casuali (in un range tra 1 e 100). Da lì parte un timer di 30 secondi.
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
